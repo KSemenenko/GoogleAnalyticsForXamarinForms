fc728ee7eee0435cf8ef24224d8ebded9d3f474e
ksemenenko <ksemenenko@live.com>
2016-08-20 17:50:11 +0300

    nuget

--- GoogleAnalytics.Shared/GoogleAnalyticsImplementation.cs
+++ GoogleAnalytics.Shared/GoogleAnalyticsImplementation.cs
@@ -38,6 +38,7 @@ namespace Plugin.GoogleAnalytics
         public void InitTracker()
         {
             TrackerFactory.Current.InitTracker(Config);
+            //Tracker. 
         }
     }
 }
