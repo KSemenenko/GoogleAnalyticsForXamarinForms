abadc404fec631e32dda039892b124438259d413
Kostiantyn Semenenko <kostiantyn.semenenko@e-consulting.com.ua>
2016-09-09 17:46:55 +0300

    update test

--- TestFormsApp/TestFormsApp/App.cs
+++ TestFormsApp/TestFormsApp/App.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Plugin.GoogleAnalytics;
 using Xamarin.Forms;
 
@@ -13,17 +12,17 @@ namespace TestFormsApp
     {
         public App()
         {
-            GAServiceManager.Current.PayloadSent += delegate(object s, PayloadSentEventArgs ev)
+            GAServiceManager.Current.PayloadSent += delegate (object s, PayloadSentEventArgs ev)
             {
                 Debug.WriteLine($"Payload sent! Response:\n{ev.Response}");
             };
 
-            GAServiceManager.Current.PayloadFailed += delegate(object s, PayloadFailedEventArgs ev)
+            GAServiceManager.Current.PayloadFailed += delegate (object s, PayloadFailedEventArgs ev)
             {
                 Debug.WriteLine($"Payload Failed! Error: {ev.Error}");
             };
 
-            GAServiceManager.Current.PayloadMalformed += delegate(object s, PayloadMalformedEventArgs ev)
+            GAServiceManager.Current.PayloadMalformed += delegate (object s, PayloadMalformedEventArgs ev)
             {
                 Debug.WriteLine($"Payload Malformed! HttpStatusCode: {ev.HttpStatusCode}");
             };
@@ -54,16 +53,17 @@ namespace TestFormsApp
 
         private void B_Clicked(object sender, EventArgs e)
         {
+
             GoogleAnalytics.Current.Config.TrackingId = "UA-11111111-1";
             GoogleAnalytics.Current.Config.AppId = "AppID";
             GoogleAnalytics.Current.Config.AppName = "TEST";
-            GoogleAnalytics.Current.Config.AppVersion = "1.0.0.0";
-            GoogleAnalytics.Current.Config.Debug = true;
+            //GoogleAnalytics.Current.Config.Debug = true;
             GoogleAnalytics.Current.InitTracker();
 
             try
             {
                 GoogleAnalytics.Current.Tracker.SendView("MainPage");
+                GoogleAnalytics.Current.Tracker.SendException(new Exception("oops"), false);
             }
             catch(Exception ex)
             {
